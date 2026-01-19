4db7939b07efb0684ba34ec897370af724d4b59d
Kishore R B <justkishore10@gmail.com>
2026-01-19 18:48:16 +0530

    Add greeting for Tiggers in App.java

--- src/main/java/views/App.java
+++ src/main/java/views/App.java
@@ -7,6 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Devops!");
+        System.out.println("Hello Tiggers!");
+
 
     }
 }
