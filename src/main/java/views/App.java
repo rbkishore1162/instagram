32501d86f28cc087b6b6dbd8fec503b6f4aac11e
Kishore R B <justkishore10@gmail.com>
2026-01-19 18:42:45 +0530

    Add greeting for DevOps

--- src/main/java/views/App.java
+++ src/main/java/views/App.java
@@ -6,5 +6,7 @@ package views;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Devops!");
+
     }
 }
